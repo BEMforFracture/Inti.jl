fa5f792dd19ba3ce3d209a0088d97c9a85c22b23
adrien vet <adrien.vet@ensta.fr>
2026-03-25 11:28:21 +0100

    added the parameter nmax

--- ext/IntiHMatricesExt.jl
+++ ext/IntiHMatricesExt.jl
@@ -14,13 +14,15 @@ function Inti.assemble_hmatrix(
         rank = typemax(Int),
         rtol = atol > 0 || rank < typemax(Int) ? 0 : sqrt(eps(Float64)),
         eta = 3,
+        nmax = 200,
     )
     comp = HMatrices.PartialACA(; rtol, atol, rank)
     adm = HMatrices.StrongAdmissibilityStd(eta)
     X = [Inti.coords(x) for x in iop.target]
     Y = iop.target === iop.source ? X : [Inti.coords(y) for y in iop.source]
-    Xclt = HMatrices.ClusterTree(X; copy_elements = false)
-    Yclt = X === Y ? Xclt : HMatrices.ClusterTree(Y; copy_elements = false)
+    splitter = HMatrices.GeometricSplitter(nmax)
+    Xclt = HMatrices.ClusterTree(X, splitter; copy_elements = false)
+    Yclt = X === Y ? Xclt : HMatrices.ClusterTree(Y, splitter; copy_elements = false)
     return HMatrices.assemble_hmatrix(iop, Xclt, Yclt; adm, comp, global_index = true)
 end
 
